82ba2b77c2a6f07eb4de3d876eaf347e897be465
small-mayfly <963626829@qq.com>
2020-05-13 12:54:59 +0800

    丰收

---
+++ src/niuke/S2019/Tqid32834.java
@@ -0,0 +1,59 @@
+package niuke.S2019;
+
+import java.util.*;
+
+public class Tqid32834 {
+    /**
+     * 对所有询问按照从小到大排序后依次计算，复杂度O(n)
+     * @param args
+     */
+    public static void main(String[] args){
+
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] as = new int[n];
+        for(int i=0;i<n;i++){
+            as[i] = in.nextInt();
+        }
+        int m = in.nextInt();
+        HashMap<Integer, Integer> qs = new HashMap<Integer, Integer>();
+        int[] qst = new int[m];
+        int[] qsrem = new int[m];
+        for(int i=0;i<m;i++){
+            qst[i] = in.nextInt();
+            qsrem[i] = qst[i];
+            qs.put(qst[i], 0);
+        }
+
+        Arrays.sort(qst);
+
+        int tq = qst[0];
+        int tindex = 1;
+        int rem = tq;
+
+        int index = 0;
+        while(index<=n){
+            if(tq>0){
+                tq -= as[index];
+                index++;
+            }else{
+                qs.put(rem, index);
+                if(tindex<m){
+                    int now = qst[tindex++];
+                    tq += (now-rem);
+                    rem = now;
+                }else{
+                    break;
+                }
+            }
+
+        }
+
+        for(int i=0;i<m;i++){
+            System.out.println(qs.get(qsrem[i]));
+        }
+
+
+    }
+}
